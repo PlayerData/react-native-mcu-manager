30da7bd9fe47363734db1df440cbeba4484888c2
alec <alec@playerdata.co.uk>
2020-08-19 12:00:31 +0100

    Allow updates to block and run one at a time

--- ios/RNMcuManager.swift
+++ ios/RNMcuManager.swift
@@ -40,8 +40,8 @@ class RNMcuManager: RCTEventEmitter {
             self.updater!.startUpdate()
         } catch {
             self.updater = nil;
-            let error = NSError(domain: "", code: 200, userInfo: nil)
-            reject("error", "failed to open file", error);
+//            let error = NSError(domain: "", code: 200, userInfo: nil)
+            reject("error", error.localizedDescription, error);
             return
         }
     }
