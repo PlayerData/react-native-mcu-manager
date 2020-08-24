ee65ccd566cdc56a27e6fc4786c5232bc5d1da87
Hayden Ball <hayden@playerdata.co.uk>
2020-08-24 17:00:41 +0100

    Prevent app crash on update start

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,38 @@
 import { NativeEventEmitter } from "react-native";
-import McuManager from ".";
 
 declare module McuManager {
   export const updateDevice: (
     macAddress: String,
-    updateFileUriString: String,
+    updateFileUriString: String
   ) => Promise<null>;
   export const cancel: () => void;
 }
 
 export const UploadEvents: NativeEventEmitter;
+
+export type FirmwareUploadStateAndroid =
+  | "NONE"
+  | "VALIDATE"
+  | "UPLOAD"
+  | "TEST"
+  | "RESET"
+  | "CONFIRM"
+  | "SUCCESS"
+  | "UNKNOWN";
+
+export type FirmwareUploadStateIOS =
+  | "none"
+  | "validate"
+  | "upload"
+  | "test"
+  | "reset"
+  | "confirm"
+  | "success";
+
+export interface ProgressEvent {
+  bleId: string;
+  state: FirmwareUploadStateIOS | FirmwareUploadStateAndroid;
+  progress: string;
+}
+
 export default McuManager;
