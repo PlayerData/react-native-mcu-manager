1879437e5fd506409dbf21226d66c776e2a02065
Hayden Ball <hayden@playerdata.com>
2022-05-03 11:41:16 +0100

    feat: Add UpgradeMode to interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
 import { NativeModules, NativeEventEmitter } from 'react-native';
 
-interface UpdateOptions {
-  /*
+export enum UpgradeMode {
+  /**
+   * This mode is the default and recommended mode for performing upgrades due to it's ability to
+   * recover from a bad firmware upgrade. The process for this mode is upload, test, reset, confirm.
+   */
+  TEST_AND_CONFIRM = 1,
+
+  /**
+   * This mode is not recommended. If the device fails to boot into the new image, it will not be able
+   * to recover and will need to be re-flashed. The process for this mode is upload, confirm, reset.
+   */
+  CONFIRM_ONLY = 2,
+
+  /**
+   * This mode is useful if you want to run tests on the new image running before confirming it
+   * manually as the primary boot image. The process for this mode is upload, test, reset.
+   */
+  TEST_ONLY = 3,
+}
+
+export interface UpdateOptions {
+  /**
    * The estimated time, in seconds, that it takes for the target device to swap to the updated image.
    */
   estimatedSwapTime: number;
+
+  /**
+   * McuManager firmware upgrades can actually be performed in few different ways.
+   * These different upgrade modes determine the commands sent after the upload step.
+   *
+   * @see UpgradeMode
+   */
+  upgradeMode?: UpgradeMode;
 }
 
 type McuManagerType = {
