affe6595cd5697aabbbca54a5b2ded43c0840545
Hayden Ball <hayden@playerdata.com>
2022-09-22 11:25:06 +0100

    feat: Expose ability to erase secondary image

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
+import { NativeModules } from 'react-native';
+
+const { McuManager } = NativeModules;
+
 import Upgrade, {
   FirmwareUpgradeState,
   UpgradeOptions,
   UpgradeMode,
 } from './Upgrade';
+
+export const eraseImage = McuManager.eraseImage as (
+  bleId: string
+) => Promise<void>;
 
 export { Upgrade, FirmwareUpgradeState, UpgradeOptions, UpgradeMode };
