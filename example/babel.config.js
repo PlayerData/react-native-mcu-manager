ec33d5603972ee293065aa278d21860bd6fb4b0b
Da.Sanchez <da.sanchez.suarez@gmail.com>
2023-10-26 09:02:11 +0100

    refactor: Prebuilding the example apps

--- example/babel.config.js
+++ example/babel.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const pak = require('../package.json');
 
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: ['babel-preset-expo'],
   plugins: [
     [
       'module-resolver',
