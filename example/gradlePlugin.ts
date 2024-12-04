23fd178cd1ab8a50634343952577d34a7dadd32f
jamesoneill997 <jamesoneill997@gmail.com>
2024-12-04 12:30:14 +0000

    chore: migrate to dynamic config, add custom gradle plugin
    fix: remove unneeded gradle.properties file

---
+++ example/gradlePlugin.ts
@@ -0,0 +1,23 @@
+import { withGradleProperties } from '@expo/config-plugins';
+import { ConfigPlugin } from '@expo/config-plugins';
+import { ExpoConfig } from '@expo/config-types';
+
+/**
+ * A Config Plugin to modify android/gradle.properties.
+ */
+const withCustomGradleProps: ConfigPlugin<ExpoConfig> = (config) => {
+  return withGradleProperties(config, (config) => {
+    const gradleProperties = config.modResults;
+
+    gradleProperties.push(
+      { type: 'property', key: "org.gradle.parallel", value: "true" },
+      { type: 'property', key: "org.gradle.daemon", value: "true" },
+      { type: 'property', key: "org.gradle.jvmargs", value: "-Xmx4g -Dfile.encoding=UTF-8" },
+      { type: 'property', key: "org.gradle.configureondemand", value: "true" }
+    );
+
+    return config;
+  });
+};
+
+export default withCustomGradleProps;
