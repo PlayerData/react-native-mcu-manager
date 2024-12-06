82004642fbf49d587fdb6e908dd2cddbe13071e5
Hayden Ball <hayden@playerdata.com>
2024-12-06 21:42:51 +0000

    feat: Explicitly set either bin or zip upgrade file
    This avoids any ambiguity.
    
    BREAKING CHANGE: UpgradeOptions must now include the upgrade file type

--- react-native-mcu-manager/src/index.ts
+++ react-native-mcu-manager/src/index.ts
@@ -1,10 +1,10 @@
 import McuManagerModule from './ReactNativeMcuManagerModule';
 import type { FirmwareUpgradeState, UpgradeOptions } from './Upgrade';
-import Upgrade, { UpgradeMode } from './Upgrade';
+import Upgrade, { UpgradeMode, UpgradeFileType } from './Upgrade';
 
 export const eraseImage = McuManagerModule?.eraseImage as (
   bleId: string
 ) => Promise<void>;
 
-export { Upgrade, UpgradeMode };
+export { Upgrade, UpgradeMode, UpgradeFileType };
 export type { FirmwareUpgradeState, UpgradeOptions };
