59f0f060b6e201db2e338c9da854a0386117a2f2
purrsong-chong <chongs02@purrsong.com>
2026-01-28 14:54:51 +0900

    feat(example): add location permission fix plugin

--- example/app.config.ts
+++ example/app.config.ts
@@ -2,35 +2,33 @@ import { ExpoConfig } from '@expo/config-types';
 import 'ts-node/register';
 
 const config: ExpoConfig = {
-  name: "react-native-mcu-manager-example",
-  slug: "react-native-mcu-manager-example",
-  assetBundlePatterns: ["**/*"],
-  orientation: "portrait",
-  platforms: ["ios", "android"],
-  scheme: "rnmcumgr",
-  version: "1.0.0",
+  name: 'react-native-mcu-manager-example',
+  slug: 'react-native-mcu-manager-example',
+  assetBundlePatterns: ['**/*'],
+  orientation: 'portrait',
+  platforms: ['ios', 'android'],
+  scheme: 'rnmcumgr',
+  version: '1.0.0',
   splash: {
-    image: ".assets/images/pd.png",
-    backgroundColor: "#FFFFFF",
+    image: '.assets/images/pd.png',
+    backgroundColor: '#FFFFFF',
   },
   ios: {
     supportsTablet: true,
-    bundleIdentifier: "uk.co.playerdata.reactnativemcumanager.example",
+    bundleIdentifier: 'uk.co.playerdata.reactnativemcumanager.example',
     infoPlist: {
-      NSBluetoothAlwaysUsageDescription: "Requires Bluetooth to perform firmware updates.",
+      NSBluetoothAlwaysUsageDescription:
+        'Requires Bluetooth to perform firmware updates.',
     },
   },
   android: {
-    package: "uk.co.playerdata.reactnativemcumanager.example",
-    permissions: [
-      "ACCESS_FINE_LOCATION",
-      "ACCESS_COARSE_LOCATION",
-    ],
+    package: 'uk.co.playerdata.reactnativemcumanager.example',
+    permissions: ['ACCESS_FINE_LOCATION', 'ACCESS_COARSE_LOCATION'],
   },
   plugins: [
-    ["expo-document-picker"],
-    ["expo-router"],
-    ["./gradlePlugin.ts"],
+    ['expo-document-picker'],
+    ['expo-router'],
+    ['./gradlePlugin.ts'],
     [
       'expo-build-properties',
       {
@@ -39,7 +37,13 @@ const config: ExpoConfig = {
         },
       },
     ],
-  ]
+    [
+      '@playerdata/react-native-mcu-manager',
+      {
+        removeLocationMaxSdkVersion: true,
+      },
+    ],
+  ],
 };
 
 export default config;
